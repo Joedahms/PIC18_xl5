ec6170ba032ba8056bd9e157d599b92793b28cf3
Joedahms <dahms.joejoe@gmail.com>
2024-03-13 12:33:07 -0500

    added a struct containing drive direction data

---
+++ build/default/production/xl5.p1.d
@@ -0,0 +1,3 @@
+build/default/production/xl5.p1:  \
+xl5.c  \
+xl5.h 
--- xl5.h
+++ xl5.h
@@ -8,6 +8,12 @@
 #ifndef XL5_H
 #define	XL5_H
 
+struct xl5_data
+{
+    uint8_t drive_dir;      // Value of drive direction (0 or 1)
+    uint8_t drive_dir_flag; // Whether drive direction has been sent/received
+};
+
 void init_XL5();
 void throttle_test();
 
